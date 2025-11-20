931ad1ae8aaa0f7c70421a4c3dd63c47eb738012
MrSody71 <usmvid66780@gail.com>
2025-11-20 17:49:33 +0300

    Создан класс студенты

---
+++ student.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, group, age):
+        self.name = name
+        self.group = group
+        self.age = age
+
+    def info(self):
+        return f"Студент: {self.name}, группа: {self.group}, возраст: {self.age}"
+
+s = Student("Артюх В.В.", "221131", 19)
+print(s.info())
